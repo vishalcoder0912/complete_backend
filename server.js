49a208aa40c89e48131eb64aa8865a263e85616e
Vishal-Mernstack <vishalkumarmahto.bcr23181@imsnoida.com>
2026-02-08 20:11:24 +0530

    some added things

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express=require('express')
+// const express=require('express')
 
-const app=express();// server instannce create kr rha ha
+// const app=express();// server instannce create kr rha ha
 
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
+// app.get('/',(req,res)=>{
+//     res.send("hello world")
+// })
+
+// app.get("/about",(req,res)=>{
+//     res.send("about page")
+// })
+
+// app.listen(3000);
+
+const app=require("./src/app")
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
 })
-
-app.get("/about",(req,res)=>{
-    res.send("about page")
-})
-
-app.listen(3000);
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+
+/*
+note ={
+title :"my first note",
+description : "these is myt first note"
+
+
+}
+const notes={
+}
+
+
+
+
+*/
+
+const notes=[]
+
+
+module.exports=app;
