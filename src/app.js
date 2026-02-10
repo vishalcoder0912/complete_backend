7a60b67e497fafd6d7fba878dd1d682588f5fe5c
Vishal-Mernstack <vishalkumarmahto.bcr23181@imsnoida.com>
2026-02-10 20:43:12 +0530

    idk just some mood off

--- src/app.js
+++ src/app.js
@@ -43,6 +43,17 @@ app.delete('/notes/:index',(req,res)=>{
 
 })
 
+app.patch('/notes/:index',(req,res)=>{
+const index=req.params.index
+const description=req.body.description
+
+notes[index].description=description
+res.status(200).json({
+    message:"note updated successfully"
+})
+
+})
+
 
 
 module.exports=app;
